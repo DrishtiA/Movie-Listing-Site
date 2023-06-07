014f04cb24a9cc63ae33a346eb33441b693d15de
Drisht Agarwal <d.agarwal@iitg.ac.in>
2023-06-07 11:58:50 +0530

    overwiew on hover feature added

--- script.js
+++ script.js
@@ -24,14 +24,35 @@ function displayMovieData(movies) {
 
     const title = document.createElement('h2');
     title.textContent = movie.title;
-
+    title.style.display = 'block'; // Ensure title is always visible
 
     const poster = document.createElement('img');
     poster.src = IMG_URL + movie.poster_path;
     poster.alt = movie.title;
 
+    const overview = document.createElement('p');
+    overview.textContent = movie.overview;
+    overview.classList.add('overview');
+    overview.style.opacity = '0';
+
+    // Event listeners for hover events
+    box.addEventListener('mouseover', () => {
+      title.style.display = 'block';
+      overview.style.display = 'block';
+      overview.style.opacity = '1';
+    });
+
+    box.addEventListener('mouseout', (event) => {
+      if (!box.contains(event.relatedTarget)) {
+        title.style.display = 'none';
+        overview.style.display = 'none';
+        overview.style.opacity = '0';
+      }
+    });
+
     box.appendChild(poster);
     box.appendChild(title);
+    box.appendChild(overview);
 
     container.appendChild(box);
   });
